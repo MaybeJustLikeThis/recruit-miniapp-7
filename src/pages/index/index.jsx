e1cd3b0c8d8de3bd04ec77b27f6da2e8aaa10356
崔鼎 <1692338302@qq.com>
2023-08-28 00:20:18 +0800

    首页球形轮播图实现

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,41 +1,67 @@
-import { View, Text } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
-import './index.scss'
-// import Swiper JS
-import Swiper from 'swiper';
-// import Swiper styles
-import 'swiper/css';
-
-var mySwiper = new Swiper('.swiper', {
-  autoplay: true,//可选选项，自动滑动
-  loop: true,
-})
-
+import { View, Text, Swiper, SwiperItem } from "@tarojs/components";
+import { useLoad } from "@tarojs/taro";
+import "./index.scss";
+import React, { useState } from 'react';
 export default function Index() {
-
   useLoad(() => {
-    console.log('Page loaded.')
-  })
+    console.log("Page loaded.");
+
+  });
+  const [currentIndex, setCurrentIndex] = useState(1);
+  const items = [
+    { id: 1, name: "红芯书院" },
+    { id: 2, name: "云顶书院" },
+    { id: 3, name: "aa书院" },
+    { id: 4, name: "bb书院" },
+    { id: 5, name: "cc书院" },
+    { id: 6, name: "dd书院" },
+  ];
+  const handleChange = (e) => {
+    console.log("当前滑块的索引", e.detail.current);
+    // currentIndex = e.detail.current;
+    if (e.detail.current === 5) {
+      setCurrentIndex(0);
+    } else {
+    setCurrentIndex(e.detail.current+1);
+      
+    }
+  };
 
   return (
     <View className="home">
       <View className="prompts">
         <h1>您好，</h1>
-        <View className="welcome">欢迎使用<h1>「云昭云曜」</h1></View>
+        <View className="welcome">
+          欢迎使用<h1>「云昭云曜」</h1>
+        </View>
         <View>请选择你所在的</View>
         <h1>「书院」</h1>
       </View>
-      <View>
-      <div class="swiper">
-  <div class="swiper-wrapper">
-    <div class="swiper-slide">slider1</div>
-    <div class="swiper-slide">slider2</div>
-    <div class="swiper-slide">slider3</div>
-  </div>
-</div>
-      </View>
 
-    
+      <Swiper
+        className="swiper"
+        indicatorColor="#999"
+        indicatorActiveColor="red"
+        vertical={false}
+        circular
+        indicatorDots={false}
+        displayMultipleItems={3}
+        current={0}
+        onChange={handleChange}
+        autoplay
+      >
+        {items.map((item, index) => {
+          let className = "swiper-slide";
+          if (index === currentIndex) {
+            className += " active";
+          }
+          return (
+            <SwiperItem key={item.id}>
+              <View className={className}>{item.name}</View>
+            </SwiperItem>
+          );
+        })}
+      </Swiper>
     </View>
-  )
+  );
 }
