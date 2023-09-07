f8e023823a1fac2db4b0045c3077bc7e84169d1f
DoubleLiHao <=2998000782@qq.com>
2023-09-07 09:54:21 +0800

    feat:报名页面WebView

--- src/pages/apply/apply.jsx
+++ src/pages/apply/apply.jsx
@@ -1,135 +1,26 @@
+/*
+ * @Author: DoubleLiHao =2998000782@qq.com
+ * @Date: 2023-08-29 11:11:31
+ * @LastEditors: DoubleLiHao =2998000782@qq.com
+ * @LastEditTime: 2023-09-07 09:19:31
+ * @FilePath: \yzyy\src\pages\apply\apply.jsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import "./apply.scss";
 
-import {
-  View,
-  Image,
-  Form,
-  Text,
-  Input,
-  Picker,
-  Button,
-} from "@tarojs/components";
-import ColumnBox from "../../Components/ColumnBox/ColumnBox";
+import { WebView } from "@tarojs/components";
 import { useDispatch, useSelector } from "react-redux";
-import { setApplyInfo, setDirectionCheck } from "../../store/applySlice";
 import Taro from "@tarojs/taro";
-import { useEffect, useState } from "react";
-import request from "../../httpService/request";
 
 function Apply() {
-  const logo =
-    "https://img-doubleli.oss-cn-hangzhou.aliyuncs.com/yundingLogo.png";
-
-  // 选择的方向
-  const direction = ["开发", "设计", "秘书处"];
   const { openid } = useSelector((state) => state.userSlice);
-  const data = useSelector((state) => state.applySlice);
 
-  // 判断是否是初次进入页面
-  const [isFirstEnter, setisFirstEnter] = useState(true);
-  useEffect(() => {
-    if (!isFirstEnter) {
-      request("/miniapp/user/save", { ...data, 'cloudId': openid }, "POST").then(res => {
-        if (res.status === '500') {
-          Taro.showToast({icon:'error', title:'信息格式有误'})
-        } else {
-          Taro.showToast({icon:'success',title:'报名成功'})
-          Taro.navigateTo({ url: "/pages/login/login" });
-        }
-      })
-    }
-  }, [data, isFirstEnter]);
-
-  const dispatch = useDispatch();
-  // 获取选择的方向并进行展示
-  const { directionCheck } = data;
-  const directionChoose = (e) => {
-    const index = e.target.value;
-    dispatch(setDirectionCheck(direction[index]));
+  const getResult = (e) => {
+    const { data } = e.detail;
+    Taro.showToast({ title: `报名成功` });
   };
-
-  // 提交表单
-  const formSubmit = (e) => {
-    e.detail.value.direction = directionCheck;
-    dispatch(setApplyInfo(e.detail.value));
-    setisFirstEnter(false);
-  };
-
-  return (
-    <View className="page">
-      <View className="info-box">
-        <ColumnBox>
-          <View className="box-title">填写你的信息</View>
-          <View className="box-icon">
-            <Image className="img" src={logo}></Image>
-          </View>
-          <View className="info-form">
-            <Form onSubmit={formSubmit}>
-              <View className="top">
-                <Text>姓名</Text>
-                <Input name="name" className="top-input"></Input>
-                <Text>性别</Text>
-                <Input name="gender" className="top-input"></Input>
-              </View>
-              <View className="flex-box">
-                <Text>学号</Text>
-                <Input
-                  name="studentNumber"
-                  className="long-input"
-                  placeholder="请填写"
-                ></Input>
-              </View>
-              <View className="flex-box">
-                <Text>方向</Text>
-                <Picker
-                  range={direction}
-                  className="select"
-                  onChange={directionChoose}
-                >
-                  {directionCheck || "请选择"}
-                </Picker>
-              </View>
-              <View className="flex-box">
-                <Text>专业班级</Text>
-                <Input
-                  name="major"
-                  className="long-input"
-                  placeholder="例：软件2139"
-                ></Input>
-              </View>
-              <View className="flex-box">
-                <Text>手机号</Text>
-                <Input
-                  name="phone"
-                  className="long-input"
-                  placeholder="请填写"
-                ></Input>
-              </View>
-              <View className="flex-box">
-                <Text>QQ</Text>
-                <Input
-                  name="qq"
-                  className="long-input"
-                  placeholder="请填写"
-                ></Input>
-              </View>
-              <View className="flex-box">
-                <Text>邮箱</Text>
-                <Input
-                  name="email"
-                  className="long-input"
-                  placeholder="请填写"
-                ></Input>
-              </View>
-              <Button formType="submit" className="submit">
-                提交
-              </Button>
-            </Form>
-          </View>
-        </ColumnBox>
-      </View>
-    </View>
-  );
+  const url = `https://ydsy.61231.cn/apply.html?openid=${openid}`;
+  return <WebView src={url} onMessage={getResult}></WebView>;
 }
 
 export default Apply;
--- src/pages/fail/fail.jsx
+++ src/pages/fail/fail.jsx
@@ -5,7 +5,7 @@ import ApplyState from '../../Components/ApplyState/ApplyState';
 export default function Fail() {
     return (
       <View>
-        <ApplyState value="似乎出了一点小问题。。" state="fail"></ApplyState>
+        <ApplyState value="学号/邮箱格式错误" state="fail"></ApplyState>
       </View>
     );
 }
